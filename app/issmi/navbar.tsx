a8b7dc617a883f6ac12121cbcc623343fb0e2c56
jacob <jacobfl2013@gmail.com>
2023-10-16 20:51:21 +1300

    added animation for mobile menu

--- app/issmi/navbar.tsx
+++ app/issmi/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import { Logo } from "./footer";
-
+import gsap from "gsap";
 
 export default function Navbar() {
     const ref = useRef<any>();
@@ -23,28 +23,47 @@ export default function Navbar() {
                     <button className="border-black p-2 rounded-full border-2 hover:border-white hover:bg-black hover:text-white">Sign in</button>
                 </div>
                 <div className="md:hidden">
-                    <HamburgerIcon setMenu={setShowMenu} />
+                    <HamburgerIcon setMenu={setShowMenu} menu={showMenu} />
                 </div>
 
             </div>
             {
-                showMenu && <div className="md:hidden w-2/5 py-4 text-2xl h-1/2 z-50 bg-gray-400 px-2 items-center justify-center space-x-7 flex-col flex -mt-4 float-right ml-auto rounded-xl bg-opacity-80">
-                    <button className="text-left  w-full px-3 py-2">Auctions</button>
-                    <button className="text-left  w-full  py-2">Sell</button>
-                    <button className="text-left  w-full  py-2">Private Sales</button>
-                    <button className="text-left  w-full  py-2">About</button>
-                    <button className="text-left  w-full  py-2">Support</button>
-                    <button className="border-black p-2 rounded-full border-2 hover:border-white hover:bg-black hover:text-white">Sign in</button>
-                </div>
+                showMenu && <MobileMenu />
             }
         </div>
 
     )
 }
 
-function HamburgerIcon({ setMenu }: { setMenu: Function }) {
+function MobileMenu() {
     return (
-        <button onClick={() => { setMenu((e: boolean) => !e) }} className="w-12 h-12">
+        <div id='mobileMenu' className="md:hidden w-3/5 py-4 text-2xl h-1/2 z-50 bg-gray-400 px-2 items-center justify-center space-x-7 flex-col flex -mt-4 float-right ml-auto rounded-xl bg-opacity-80 mobileMenu">
+            <button className="text-left  w-full px-3 py-2">Auctions</button>
+            <button className="text-left  w-full  py-2">Sell</button>
+            <button className="text-left  w-full  py-2">Private Sales</button>
+            <button className="text-left  w-full  py-2">About</button>
+            <button className="text-left  w-full  py-2">Support</button>
+            <button className="border-black p-2 rounded-full border-2 hover:border-white hover:bg-black hover:text-white">Sign in</button>
+        </div>
+    )
+}
+
+function HamburgerIcon({ setMenu, menu }: { setMenu: Function, menu: boolean }) {
+    function handleClick() {
+        if (menu) {
+            //Want to fade out elemenet
+            gsap.to(document.getElementById('mobileMenu'), {
+                translateX: 200, duration: 0.2, onComplete: () => {
+                    setMenu((e: boolean) => !e)
+                }
+            })
+
+        } else {
+            setMenu((e: boolean) => !e)
+        }
+    }
+    return (
+        <button onClick={() => { handleClick() }} className="w-12 h-12">
             <div className="h-1 w-3/4 rounded-xl bg-gray-400 my-2"></div>
             <div className="h-1 w-3/4 rounded-xl bg-gray-400 my-2"></div>
             <div className="h-1 w-3/4 rounded-xl bg-gray-400 my-2"></div>
