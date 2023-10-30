9c8b7a66d594d03a7b815696714bdcb894d43416
jacob <jacobfl2013@gmail.com>
2023-10-31 10:11:40 +1300

    Added more listings & private sales

--- app/issmi/auctions/listing.ts
+++ app/issmi/auctions/listing.ts
@@ -5,14 +5,14 @@ export const listingData: Listing[] = [
         src: '1969MB280SE.jpg',
         title: '1969 Mercedes-Benz 280SE Cabriolet',
         year: "1969",
-        make: ['mercedes', 'mb', 'mercedes-benz', 'mercedes benz', 'benz', 'merc', 'cabriolet', 'cabrio', '280e', '280se'],
+        make: ['mercedes', 'mb', 'mercedes-benz', 'mercedes benz', 'benz', 'merc', 'cabriolet',  '280e', '280se'],
         description: " Beautiful Blue- Gray With a Blue Convertible Top and Blue Leather Interior, 4 - Speed Automatic Gearbox, Air Conditioning, Power Steering.",
         asking: 139000
     }, { 
         src: '1971Porsche22Targa.jpg',
         title: '1971 Porsche 911 S 2.2 Targa',
         year: "1971",
-        make: ['porsche', 'targa', '911', '2.2'], 
+        make: ['porsche', 'targa', '911', '2.2', 'convertible'], 
         description: 'Freshly Restored 2.2-Liter 911 S Targa, Finished in Albert Blue Over Black Interior, Comprehensive Mechanical and Cosmetic Reconditioning.',
         asking: 1652750
     }, { 
@@ -22,5 +22,72 @@ export const listingData: Listing[] = [
         make: ['porsche', '911', 'gt3', '4.0', '4l'],
         description: "The Ultimate 997.2 GT3 RS Finished in Carrara White Over Black and Red Alcantara/ Leather Interior, Car 374 / 600 Produced Worldwide, Fully Serviced and Ready to Enjoy.",
         asking: 409500
+    }, { 
+        src: '2008F430Scuderia.jpg',
+        title: "2008 Ferrari F430 Scuderia",
+        year: '2008',
+        make: ['ferrari', 'f430', 'scuderia'],
+        description: "Rosso Corsa With Grigio Silverstone Stripes Over Black Cloth and Alcantara Interior, Radio Delete, 35,736 km Shown With a Full Service History.",
+        asking: 299250,
+    }, { 
+        src: '2012Ferrari450Spider.jpg',
+        title: '2012 Ferrari 458 Spider',
+        year: '2012',
+        make: ['ferrari', '458', 'f458', 'spider'], 
+        description: "Giallo Triplo Strato Over Black Leather With Yellow Stitching, Showing 13,000 km, Freshly Serviced and Fitted With Scuderia Shields and Carbon Wheel With LED Shift Lights.",
+        asking: 262500
+    }, { 
+        src: '1996Porsche911Carrera.jpg',
+        title: '1996 Porsche 911 Carrera Cabriolet',
+        year: '1996',
+        make: ['porsche', '911', 'carrera', 'cabriolet', '993'],
+        description: 'Paint to Sample Forest Green Metallic Over Cashmere Supple Leather, Beautiful 993 Showing 125,110 KM and Fitted With a 6-Speed Manual, Limited Slip Diff and Motor Sound Package.',
+        asking: 78000
+    }, { 
+        src: '2022PorscheGT3.jpg',
+        title: "2022 Porsche 911 GT3",
+        year: '2022',
+        make: ['porsche', 'gt3', '911', '992'],
+        description: "Guards Red over Black Leather and Race-Tex 992 GT3, 7-Speed PDK Gearbox, 6,006 Miles, Full Bucket Seats, Bose Surround Sound, and PCCBs.",
+        asking: 265000
+    }, { 
+        src: "1998FerrariMaranello.jpg",
+        title: "1998 Ferrari 550 Maranello",
+        year: "1998",
+        make: ['ferrari', '550', 'maranello', 'f550'], 
+        description: "Argento Nurburgring Over Pelle Leather Interior, 66,850 km Shown on the Odometer and Fitted With a Gated 6-Speed Manual Transmission.",
+        asking: 145000
+    }, { 
+        src: "1996Porsche911Carrera4s.jpg",
+        title: "1996 Porsche 911 Carrera 4S",
+        year: "1996",
+        make: ['porsche', '911', '993', 'carrera', '4s'],
+        description: "One Owner Black/Black 993 Carrera 4S, Upgraded With 3.8-Liter Pistons, Euro-Carrera RS Cams, Bilstein PSS9s and 996 GT3 Sport Seats.",
+        asking: 135000
+    }
+]
+
+export const privateListingData: Listing[] = [ 
+    {
+        src: "1970AlfaRomeo1750.jpg",
+        title: "1970 Alfa Romeo 1750 GTAm",
+        year: '1970',
+        make: ['alfa romeo', 'alfa', '1750', 'gtam', 'romeo'],
+        description: "Scuderia Jolly Gancia Team Car, Winner of 1970 Interlagos 1,000 Miles and 1971 Interlagos 12 hr, Autodelta Built Car With Two Owners, Recent Cosmetic Reconditioning.",
+        asking: 0,
+    }, { 
+        src: '1990FerrariF40.jpg',
+        title: "1990 Ferrari F40",
+        year: "1990",
+        make: ['f40', 'ferrari'],
+        description: "The Ultimate Ferrari Road Car, Original Owner From New and Showing 3,739 KM, Exceptional Condition Throughout.",
+        asking: 0,
+    }, { 
+        src: '2018AstonMartinVanquish.jpg',
+        title: "2018 Aston Martin Vanquish Zagato Speedster",
+        year: "2018",
+        make: ['aston martin', 'aston', 'martin', 'vanquish', 'zagato', 'speedster'], 
+        description: "One of Just 28 Zagato Speedsters Built and Showing Just 325 km From New. An Exceptionally Unique Car In An Eye Catching Specification.",
+        asking: 0
     }
 ]
--- app/issmi/auctions/page.tsx
+++ app/issmi/auctions/page.tsx
@@ -2,7 +2,8 @@
 
 import { Listing } from "@/types";
 import { useEffect, useState } from "react"
-import { listingData } from "./listing";
+import { listingData, privateListingData } from "./listing";
+import { ButtonSVG } from "../whatWeDo";
 
 export default function AuctionPage() {
 
@@ -21,28 +22,66 @@ export default function AuctionPage() {
     }
 
     return (
-        <div className="w-full h-auto mt-36">
-            <input onChange={(e) => { e.preventDefault(); searchList(e.target.value) }}></input>
-            <div className="grid grid-cols-3 mx-6 gap-4">
+        <div className="w-full h-auto mt-36 flex px-6  flex-col">
+            <div className="w-2/3 mb-6 h-16 rounded-3xl flex items-center bg-white px-2">
+                <SearchIcon />
+                <input className="w-full h-full overflow-hidden rounded-3xl mx-2 outline-none" placeholder="Search by model, make, or year" onChange={(e) => { e.preventDefault(); searchList(e.target.value) }}></input>
+            </div>
+            <div className="grid grid-cols-3 gap-y-8 gap-x-4 ">
                 {list.map((l, i) => {
-                    return <Listing listing={l} key={i} />
+                    return <Listing privateSale={false} listing={l} key={i} />
+                })}
+            </div>
+            <PrivateSales />
+            <PrivateSalesAd />
+        </div>
+    )
+}
+
+function Listing({ listing, privateSale }: { listing: Listing, privateSale: boolean }) {
+    return (
+        <div className="text-left inline-flex flex-col space-y-4  ">
+
+            <div className="h-24 md:h-64 2xl:h-96 w-full">
+                <img className="w-full h-full object-cover rounded-xl" src={`/issmi/listings/${listing.src}`}></img>
+            </div>
+
+            <p className="font-bold text-xl">{listing.title}</p>
+            <p className="text-gray-800 leading-relaxed text-lg mr-8">{listing.description}</p>
+
+            <div className="flex  w-full h-full flex-col justify-end">
+                <p className={"text-center text-white py-2   rounded-2xl" + (privateSale ? " w-[30%] bg-[#A98000]" : "  w-[45%] bg-green-800")}>{privateSale ? "Private Sale" : `Seller asking £${listing.asking}`}</p>
+            </div>
+        </div>
+    )
+}
+
+function SearchIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none">
+            <path d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
+
+function PrivateSales() {
+    return (
+        <div className="flex mt-24 flex-col w-full h-auto">
+            <p className="text-3xl tracking-wide">Access our private listings by joining our <span className="underline text-[#A98000]">VIP Program</span></p>
+            <div className="grid grid-cols-3 gap-y-6 gap-x-4 my-6">
+                {privateListingData.map((l, i) => {
+                    return <Listing privateSale={true} listing={l} key={i} />
                 })}
             </div>
         </div>
     )
 }
 
-function Listing({ listing }: { listing: Listing }) {
+function PrivateSalesAd() {
     return (
-        <div className="text-left inline-flex flex-col space-y-4 justify-between">
-            <div className="h-24 md:h-64 2xl:h-96 w-full">
-                <img className="w-full h-full object-cover rounded-xl" src={`/issmi/listings/${listing.src}`}></img>
-            </div>
-            <p className="font-bold text-xl">{listing.title}</p>
-            <p className="text-gray-800 leading-relaxed text-lg mr-8">{listing.description}</p>
-
-            <p className="w-[45%] text-center text-white bg-green-800 py-2  rounded-2xl ">Seller asking ${listing.asking}</p>
-
+        <div className="flex border-[1px] bg-white  border-[#A98000] w-[80%] mx-auto flex-col items-center justify-center py-12">
+            <p className="text-xl font-bold">Want to see all of our exclusive listings? Access these listings by joining Private Sales.</p>
+            <ButtonSVG color1="#A98000" color2='white' />
         </div>
     )
 }
