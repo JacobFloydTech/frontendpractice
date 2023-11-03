b92635acb37916f4d70bbb22efe672fbc658cd69
jacob <jacobfl2013@gmail.com>
2023-11-04 12:09:11 +1300

    Completed grid section

--- app/issmi/private-sales/page.tsx
+++ app/issmi/private-sales/page.tsx
@@ -16,17 +16,101 @@ export default function PrivateSalesPage() {
                 </div>
                 <Grid />
             </div>
+
         </div>
     )
 }
 
 function Grid() {
     return (
-        <div className="grid grid-cols-privateSalesGrid gap-4">
-            <div className="p-2 border-white border-2 col-start-2">View all cars</div>
-            <div className="p-2 border-white border-2">Registered</div>
-            <div className="p-2 border-white border-2">Private Sales Members</div>
+        <div className="privateSalesGrid w-1/2 gap-y-4">
+            <p className="font-semibold col-start-2 text-center text-gray-400">View all cars</p>
+            <p className="font-semibold text-center">Registered</p>
+            <p className="font-semibold text-center text-[#987404]">Private Sales Member</p>
+            <BorderBottom />
+            <p>View all cars</p>
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <BorderBottom />
+            <p>View inspection reports</p>
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <BorderBottom />
+            <p>View vehicle history</p>
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <BorderBottom />
+            <p>Place offer/bid</p>
+            <Dash />
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <BorderBottom />
+            <p>Get auciton updates</p>
+            <Dash />
+            <SVGTick color={'#FFFFFF'} />
+            <SVGTick color={'#FFFFFF'} />
+            <BorderBottom />
+            <p>Access to special cars not listed anywhere</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+            <p>Collection management support</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+            <p>Access to ISSIMI staff for VIP support in transactions	</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+            <p>Import/export support</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+            <p>Invitation to exclusive events</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+            <p>Exclusive ISSIMI merch</p>
+            <Dash />
+            <Dash />
+            <SVGTick color={'#987404'} />
+            <BorderBottom />
+        </div>
+    )
+}
 
+function BorderBottom() {
+    return (
+        <div className="col-span-4 h-[1px] w-full bg-white">
+
+        </div>
+    )
+}
+
+function SVGTick({ color }: { color: string }) {
+    return (
+        <div className="flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill={color} height="15" width="15" version="1.1" id="Capa_1" viewBox="0 0 490 490" >
+                <polygon points="452.253,28.326 197.831,394.674 29.044,256.875 0,292.469 207.253,461.674 490,54.528 " />
+            </svg>
+        </div>
+    )
+}
+
+function Dash() {
+    return (
+        <div className="flex items-center justify-center">
+            <svg width={15} height={15}>
+                <rect x='50%' y='50%' width='80%' height='10%' fill="white" />
+            </svg>
         </div>
     )
 }
